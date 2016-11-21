4bfc1a736a85796edcf5b467383716b9fd8ea986
Bruno Morandin Figueiredo <Bruno Morandin Figueiredo>
2016-11-21 13:44:11 -0600

    Fixed the season rainfall table

--- class_project.R
+++ class_project.R
@@ -29,15 +29,24 @@ yld_t_ha <- yield_data %>%
   mutate(yield_t_ha = round((yield * .0673), 2)) %>%
   select(year, plant_date,harvest_date, yield_t_ha)
 
+for(i in 1:nrow(yld_t_ha)){
+  yld_t_ha$season_rainfall_mm[i] <- 
+    filter(rainfall_mm,date >= yld_t_ha$plant_date[i] & date <= yld_t_ha$harvest_date[i]) %>%
+    select(.,rainfall_mm) %>%
+    sum(.)
+}
+
 # merging raifall and yield data
-master_data <- merge(yld_t_ha,rainfall_mm, by ="year")
+master_data <- merge(yld_t_ha,rainfall_mm, by ="year", all = TRUE)
 
 # calculating season rainfall
-master_datav2 <- master_data %>%
-  group_by(date >= plant_date & date <= harvest_date) %>%
-  mutate(season_rainfall_mm = sum(rainfall_mm)) %>%
-  select(year, plant_date, harvest_date, season_rainfall_mm, yield_t_ha)
-
+master_datav2 <-
+  master_data %>%
+  filter(date >= plant_date & date <= harvest_date) %>%
+  group_by(plant_date,harvest_date,yield_t_ha) %>%
+  head
+#  summarize(season_rainfall_mm = sum(rainfall_mm))
+  
 #---------------------------------------------- Analyzing the data -------------------------------------------
 #plot yield data over years
 ggplot(master_data, aes(x = year, y = yield_t_ha)) + geom_line() + xlab("Year") + ylab("Yield (T/ha)" )
